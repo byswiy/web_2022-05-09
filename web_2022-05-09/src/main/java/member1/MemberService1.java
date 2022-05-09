afb871d587a07c40e778e4aaf395067f752ca2ac
byswiy <byswiy99@naver.com>
2022-05-10 01:45:31 +0900

    MemberService1.java : select 기능을 할 service 생성

--- web_2022-05-09/src/main/java/member1/MemberService1.java
+++ web_2022-05-09/src/main/java/member1/MemberService1.java
@@ -14,4 +14,15 @@ public class MemberService1 {
 			return 400;
 		}
 	}
+	
+	public int selectService(MemberInfo memberInfo) {
+		MemberInfoDao1 dao = new MemberInfoDao1();
+		boolean exist = dao.select(memberInfo);
+		
+		if(exist) {
+			return 409;
+		}
+		
+		return 200;
+	}
 }
