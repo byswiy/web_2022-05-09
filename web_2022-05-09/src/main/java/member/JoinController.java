34fd791d632a18d210546642d58087b0613fa57c
byswiy <byswiy99@naver.com>
2022-05-10 00:37:41 +0900

    JoinController.java : 파라미터 검증 코드 추가, 전달 받은 파라미터를 합해주는 코드 추가

--- web_2022-05-09/src/main/java/member/JoinController.java
+++ web_2022-05-09/src/main/java/member/JoinController.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import util.Validator;
+import vo.MemberInfo;
+
 @WebServlet("/member/join1")
 public class JoinController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -22,6 +25,40 @@ public class JoinController extends HttpServlet {
 	LocalDateTime joinDate = LocalDateTime.now();
 	
 	// 파라미터 검증
+	Validator validator = new Validator();
+	if(!validator.idValidator(id)) {
+		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		return;
+	} else if(!validator.pwValidator(pw)) {
+		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		return;
+	} else if(!pw.equals(pwChk)) {
+		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		return;
+	} else if(!validator.nameValidator(name)) {
+		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		return;
+	} else if(!validator.telvalidator(tel)) {
+		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		return;
+	} else if(!validator.addrValidator(addr)) {
+		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		return;
+	} else if(!validator.emailValidator(email)) {
+		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		return;
+	} 
+	
+	System.out.println("id -> " + id);
+	System.out.println("pw -> " + pw);
+	System.out.println("pwChk -> " + pwChk);
+	System.out.println("name -> " + name);
+	System.out.println("tel -> " + tel);
+	System.out.println("addr -> " + addr);
+	System.out.println("email -> " + email);
+	
+	// 전달 받은 값 뭉치기
+	MemberInfo memberInfo = new MemberInfo(id, pw, name, tel, addr, email, joinDate);
 	
 	}
 
