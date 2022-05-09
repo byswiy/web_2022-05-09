46f84d701713440d6787948358afa2c60f242b71
byswiy <byswiy99@naver.com>
2022-05-10 00:48:53 +0900

    MemberService.java : 회원가입을 위한 insertDao를 가진 service 생성

---
+++ web_2022-05-09/src/main/java/member/MemberService.java
@@ -0,0 +1,17 @@
+package member;
+
+import dao.MemberInfoDao;
+import vo.MemberInfo;
+
+public class MemberService {
+	public int join(MemberInfo memberInfo) {
+		MemberInfoDao dao = new MemberInfoDao();
+		boolean result = dao.insert(memberInfo);
+		
+		if(result) {
+			return 200;
+		} else {
+			return 400;
+		}
+	}
+}
